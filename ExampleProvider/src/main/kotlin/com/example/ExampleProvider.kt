fe2f5a90cc4edd0437a9fe0038226e285f50d237
lechanhct-blip <lechanhct@gmail.com>
2026-09-12 21:23:11 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,16 +1,26 @@
 package com.example
 
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import kotlinx.coroutines.coroutineScope
+import org.jsoup.nodes.Element
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 
 class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
-
-    override var lang = "en"
-
+  override var name = "Example provider"
+    override val supportedTypes = setOf(TvType.NSFW)
+    override val hasDownloadSupport   = true
+    override val hasChromecastSupport = true
+    override var lang = "vi"
+    override val vpnStatus            = VPNStatus.MightBeNeeded
     // Enable this when your provider has a main page
     override val hasMainPage = true
 
