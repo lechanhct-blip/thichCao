aebf77de1ff18e79e25a9d7e9b94a33ecd4c43c3
lechanhct-blip <lechanhct@gmail.com>
2026-09-13 21:59:06 +0700

    Update build.gradle.kts

--- NangCuc/build.gradle.kts
+++ NangCuc/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
 
     tvTypes = listOf("NSFW")
     //dong nay de kiem tra thu muc RES
-    requiresResources = true
+    //requiresResources = true
     language = "vi"
 
     // Random CC logo I found
