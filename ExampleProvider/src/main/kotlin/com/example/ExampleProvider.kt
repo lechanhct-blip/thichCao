04add229ecd092342f53e1527ee31775d1f917c1
lechanhct-blip <lechanhct@gmail.com>
2026-09-13 12:17:04 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -18,7 +18,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 
 class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-  override var name = " Cao Caaakkkkka"
+  override var name = " Cao CaP"
     override val supportedTypes = setOf(TvType.NSFW)
     override val hasDownloadSupport   = true
     override val hasChromecastSupport = true
