dff3e5c6ab70473aefe1f820cefecd8de4eb91dd
lechanhct-blip <lechanhct@gmail.com>
2026-09-13 11:29:25 +0700

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 }
 
 // Use an integer for version numbers
-version = 26120922
+version = 26121011
 
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
