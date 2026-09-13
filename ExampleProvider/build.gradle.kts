b404f223a4201506affefeba05d3cc4e49b78159
lechanhct-blip <lechanhct@gmail.com>
2026-09-13 21:52:25 +0700

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -21,6 +21,7 @@ cloudstream {
     status = 1 // Will be 3 if unspecified
 
     tvTypes = listOf("NSFW")
+    //dong nay de kiem tra thu muc RES
 
     requiresResources = true
     language = "vi"
