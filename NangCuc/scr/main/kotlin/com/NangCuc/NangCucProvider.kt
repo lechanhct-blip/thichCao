cf280ca1c888f3c74b890c8ff91e9b333282c28e
lechanhct-blip <lechanhct@gmail.com>
2026-09-13 21:00:02 +0700

    Update NangCucProvider.kt

--- NangCuc/scr/main/kotlin/com/NangCuc/NangCucProvider.kt
+++ NangCuc/scr/main/kotlin/com/NangCuc/NangCucProvider.kt
@@ -1,1 +1,23 @@
+package com.NangCuc
 
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import kotlinx.coroutines.coroutineScope
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.Document
+import org.jsoup.Jsoup
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+
+class NangCuc : MainAPI() {
+  
+}
