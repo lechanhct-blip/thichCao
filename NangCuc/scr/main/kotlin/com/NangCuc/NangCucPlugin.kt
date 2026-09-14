98bb86236495cf70f7788dfd811670bda3ac17a3
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 07:16:50 +0700

    Update NangCucPlugin.kt

--- NangCuc/scr/main/kotlin/com/NangCuc/NangCucPlugin.kt
+++ NangCuc/scr/main/kotlin/com/NangCuc/NangCucPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class DongGoiPluginz: Plugin() {
+class NangCucPlugin: Plugin() {
         override fun load(context: Context) {
         // All providers should be added in this manner
         //registerMainAPI(NangCuc())
