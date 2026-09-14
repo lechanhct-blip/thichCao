76814865946a2444b814cae479ac588753400edd
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 16:12:18 +0700

    Update build.gradle.kts

--- NangCuc/build.gradle.kts
+++ NangCuc/build.gradle.kts
@@ -14,8 +14,8 @@ cloudstream {
     * 3: Beta-only
     **/
     status = 1 // Will be 3 if unspecified
-    types = setOf(com.lagradost.cloudstream3.TvType.NSFW)
-    //tvTypes = listOf("NSFW")
+    
+    tvTypes = listOf("NSFW")
     //dong nay de kiem tra thu muc RES
     //requiresResources = false
     language = "vi"
