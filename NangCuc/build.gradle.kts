687ccaa2b6b85ee4f0c140b580b2f672cf2b5668
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 15:55:07 +0700

    Update build.gradle.kts

--- NangCuc/build.gradle.kts
+++ NangCuc/build.gradle.kts
@@ -4,7 +4,7 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove any of them.
     description = "Nang Cuc"
-    authors = listOf("lechanh", "ngoctan")
+    authors = listOf("lechanh")
 
     /**
     * Status int as one of the following:
