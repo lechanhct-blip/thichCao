4fff00402395c5a7f9e45c51ee66ce7732b87698
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 18:06:58 +0700

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class DongGoiPlugin: Plugin() {
+class ExamplePlugin: Plugin() {
         override fun load(context: Context) {
         // All providers should be added in this manner
         registerMainAPI(JAVHDZ())
