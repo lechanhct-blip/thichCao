39daec214e23b999db9f14164dca437b4cda659e
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 17:11:09 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -30,6 +30,7 @@ class JAVHDZ : MainAPI() { // All providers must be an instance of MainAPI
  override var mainUrl = "https://javhdz.ac"
 
     override val mainPage = mainPageOf(
+        "1" to "ĐỀ XUẤT",
         "/video/" to "video",
         "/trending/" to "trending",
         "/category/censored-2/" to "censored",
@@ -38,7 +39,32 @@ class JAVHDZ : MainAPI() { // All providers must be an instance of MainAPI
     )
 
  override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl${request.data}page/$page").document
+   var document: Document? = null
+        if (request.data!="1") {
+            document = app.get("$mainUrl${request.data}page/$page").document
+        }else{
+            document = app.get("$mainUrl/video/page/$page").document
+            //lay tông trang JAVHD
+            val docNavi = document.selectFirst("div.navigation")
+            val number = docNavi?.select("a.page-numbers")?.get(4)?.text()
+            //val number = Regex("""\.\.\.\s*(\d+)""").find(pageList?:"")?.groupValues?.get(1)
+
+// Kết quả: "70" (kiểu String) hoặc null nếu không thấy
+
+// Ép kiểu sang Int để sử dụng
+            val totalPage = number?.toIntOrNull()?:3 // 70
+            //val nn = pageNumber
+            // val numberrandom = (1..100 ).random()
+
+
+            //val pageNumber = number?.toIntOrNull() ?: 100
+            val nPage = (3..(totalPage-3).coerceAtLeast(3)).random()
+
+
+            document = app.get("$mainUrl/video/page/$nPage").document
+        }
+        
+        //val document = app.get("$mainUrl${request.data}page/$page").document
 //        val responseList  = document.select(".thumbnail").mapNotNull { it.toSearchResult() }
         val responseList  = document.select("a.movie-item").mapNotNull { it.toSearchResult() }
         return newHomePageResponse(HomePageList(request.name, responseList, isHorizontalImages = true),hasNext = true)
