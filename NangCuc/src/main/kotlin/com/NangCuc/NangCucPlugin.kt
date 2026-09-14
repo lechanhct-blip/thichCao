0883a8bbe05459a85597426537e5a4783843ac59
lechanhct-blip <lechanhct@gmail.com>
2026-09-14 22:32:58 +0700

    Create NangCucPlugin.kt

---
+++ NangCuc/src/main/kotlin/com/NangCuc/NangCucPlugin.kt
@@ -0,0 +1,13 @@
+package com.NangCuc
+import android.content.Context
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class NangCucPlugin: Plugin() {
+        override fun load(context: Context) {
+        // All providers should be added in this manner
+        registerMainAPI(NangCuc())
+    }
+}
