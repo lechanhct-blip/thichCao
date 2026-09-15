bdd2f4b023df2f0c947a1d8a8ab52796db1639e4
lechanhct-blip <lechanhct@gmail.com>
2026-09-16 00:40:17 +0700

    Update JAVHDZProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/JAVHDZProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/JAVHDZProvider.kt
@@ -92,7 +92,7 @@ private fun Element.toSearchResult(): SearchResponse? {
 
         val luot_xem = this.selectFirst(".meta-viewed")?.text()
         tenphim = "$tenphim | $luot_xem lượt xem"
-        return newMovieSearchResponse(tenphim, duong_dan, TvType.NSFW) {
+        return newAnimeSearchResponse(tenphim, duong_dan, TvType.NSFW) {
             this.posterUrl = hinh
             //this.po  = get(luot_xem)
         }
