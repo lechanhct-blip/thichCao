2e8b42e658e95cddf5af1fb5fca8ab72cf43ebde
lechanhct-blip <lechanhct@gmail.com>
2026-09-16 16:13:07 +0700

    Update MissAV.kt

--- MissAV/src/main/kotlin/com/MissAV/MissAV.kt
+++ MissAV/src/main/kotlin/com/MissAV/MissAV.kt
@@ -151,7 +151,7 @@ val headers = mapOf(
 
 
     
-    return newMovieLoadResponse(ten_phim, url, TvType.Movie, url) {
+    return newMovieLoadResponse(ten_phim, url, TvType.NSFW, url) {
         this.posterUrl = hinh
         this.plot = thong_tin
     }
